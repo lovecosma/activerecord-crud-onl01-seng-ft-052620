6582b531b167bc675f0f49c1fd163e8791b4daa3
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-27 22:52:03 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,11 @@
 require 'bundler/setup'
+require 'rake'
+require 'active_record'
+require 'yaml/store'
+require 'ostruct'
+require 'date'
+require_relative '../app/models/movie.rb'
+
 Bundler.require
 
 ActiveRecord::Base.establish_connection(
@@ -7,3 +14,16 @@ ActiveRecord::Base.establish_connection(
 )
 
 require_all 'app'
+
+sql = <<-SQL
+CREATE TABLE IF NOT EXISTS movies (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  release_date INTEGER,
+  director TEXT,
+  lead TEXT,
+  in_theaters BOOLEAN
+  )
+SQL
+
+ActiveRecord::Base.connection.execute(sql)
